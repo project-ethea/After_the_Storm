32aff047b342fd5318f74ed65c60d4aac84d9236
Ignacio R. Morelle <shadowm@wesnoth.org>
2012-01-06 00:39:39 +0000

    AtS E2S10: minor progress, including the [deactivate_and_serialize_sides] and [unserialize_and_activate_sides] WML actions implemented in Lua
    [Wesnoth-UMC-Dev SVN r12754]

--- lua/After_the_Storm.lua
+++ lua/After_the_Storm.lua
@@ -1,6 +1,8 @@
-----------------
--- SCENARIO 9 --
-----------------
+local helper = wesnoth.require "lua/helper.lua"
+
+----------
+-- E1S9 --
+----------
 
 function spawn_loyal_player_unit(type_id, loc_x, loc_y)
 	-- Inelegant, I know, but put_unit() invokes a code path
@@ -40,3 +42,56 @@ function wesnoth.wml_actions.s9_area_spawns(cfg)
 		table.remove(locs, r)
 	end
 end
+
+-----------
+-- E2S10 --
+-----------
+
+function wesnoth.wml_actions.deactivate_and_serialize_sides(cfg)
+	local variable = cfg.variable or "sides"
+	local array_index = 0
+
+	wesnoth.set_variable(variable)
+
+	for t, side_number in helper.get_sides(cfg) do
+		wesnoth.message("WML", string.format("store side %u", side_number))
+		local side_store = string.format("%s[%u]", variable, array_index)
+
+		wesnoth.wml_actions.store_side {
+			variable = side_store, side = side_number
+		}
+		wesnoth.wml_actions.store_unit {
+			kill = true,
+			variable = side_store .. ".units", { "filter", { side = side_number } }
+		}
+		wesnoth.wml_actions.modify_side {
+			controller = "null", income = -2, gold = 0, village_gold = 0,
+			hidden = true, side = side_number
+		}
+
+		array_index = array_index + 1
+	end
+end
+
+function wesnoth.wml_actions.unserialize_and_activate_sides(cfg)
+	local variable = cfg.variable or helper.wml_error("[unserialize_and_activate_sides]: Missing variable!")
+
+	local data_set = helper.get_variable_array(variable)
+
+	for index, side_data in ipairs(data_set) do
+		wesnoth.wml_actions.modify_side {
+			side = side_data.side, gold = side_data.gold, village_gold = side_data.village_gold,
+			income = side_data.income, controller = side_data.controller, hidden = false
+		}
+
+		local units = helper.get_variable_array(variable .. string.format("[%u].units", index - 1))
+
+		for uindex, container in ipairs(units) do
+			wesnoth.wml_actions.unstore_unit {
+				variable = string.format("%s[%u].units[%u]", variable, index - 1, uindex - 1),
+				find_vacant = true
+			}
+		end
+	end
+end
+
