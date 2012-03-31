3bb8b703484fd7df4107995a6bc3bd02c5929d81
Ignacio R. Morelle <shadowm@wesnoth.org>
2012-03-31 09:40:15 +0000

    AtS: use a custom name for the safe_random() WML variable so it doesn't clobber $random as normally used with the RANDOM macro
    [Wesnoth-UMC-Dev SVN r14046]

--- lua/common.lua
+++ lua/common.lua
@@ -6,12 +6,12 @@ local helper = wesnoth.require "lua/helper.lua"
 
 function safe_random(arg)
 	wesnoth.fire("set_variable", {
-		name = "random",
+		name = "temp_ats_lua_random",
 		rand = arg,
 	})
 
-	local r = wesnoth.get_variable("random")
-	wesnoth.set_variable("random")
+	local r = wesnoth.get_variable("temp_ats_lua_random")
+	wesnoth.set_variable("temp_ats_lua_random")
 
 	return r
 end
