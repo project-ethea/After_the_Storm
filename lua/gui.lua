3f50e9865de34e5ef4a8cd14eb88b64b3538caac
Ignacio R. Morelle <shadowm@wesnoth.org>
2012-08-24 09:31:49 +0000

    AtS: implement hackish [top_message] WML action
    [Wesnoth-UMC-Dev SVN r15707]

--- lua/gui.lua
+++ lua/gui.lua
@@ -95,6 +95,60 @@ function wesnoth.wml_actions.transient_message(cfg)
 end
 
 ---
+-- Displays a simple transient message at the top, without any
+-- captions or images.
+--
+-- [top_message]
+--     message=(tstring)
+-- [/top_message]
+---
+function wesnoth.wml_actions.top_message(cfg)
+	local dd = {
+		definition = "message",
+
+		vertical_placement = "top",
+		horizontal_placement = "center",
+
+		-- TODO: we can't use manual placement yet because then the width and height
+		--       become static and the game fails if the text doesn't fit later.
+		--
+		-- automatic_placement = false,
+		-- x = "(screen_width / 2)",
+		-- the menu bar height (26 px) plus the standard margin size
+		-- y = "(26 + 5)",
+		-- width = 200,
+		-- height = 100,
+
+		maximum_width = 800,
+		maximum_height = 600,
+
+		click_dismiss = true,
+
+		T.helptip { id="tooltip_large" },
+		T.tooltip { id="tooltip_large" },
+
+		T.grid {
+			T.row {
+				T.column {
+					border = "all", border_size = 20,
+					horizontal_alignment = "center",
+					vertical_alignment = "center",
+					T.label { id = "message", definition = "default" }
+				}
+			}
+		}
+	}
+
+	local message = cfg.message or ""
+
+	local function preshow()
+		wesnoth.set_dialog_value(message, "message")
+	end
+
+	wesnoth.show_dialog(dd, preshow, nil)
+end
+
+---
 -- Displays an image on a popup dialog.
 --
 -- [show_image]
