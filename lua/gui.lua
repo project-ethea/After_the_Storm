36bbb2e25b9515c8b0a49fd973a2531514c6bca3
Ignacio R. Morelle <shadowm@wesnoth.org>
2012-08-29 10:47:44 +0000

    AtS: implement [item_choice_dialog] Lua/GUI2 WML action
    This one was quite tricky, as it makes use of the unsupported
    [multi_page] widget. For all I know it might break later in trunk, so a
    [message]-based fallback might come in handy.
    
    It may or may not be replay/MP-friendly.
    
    [Wesnoth-UMC-Dev SVN r15813]

--- lua/gui.lua
+++ lua/gui.lua
@@ -429,3 +429,268 @@ function wesnoth.wml_actions.character_action_dialog(cfg)
 
 end
 
+---
+-- [item_choice_dialog]
+--     title=(string) (optional)
+--     text=(string) (optional)
+--
+--     can_dismiss=(boolean) (optional, defaults to true)
+--     variable=(string) (optional, defaults to "choice" and only makes sense
+--                       when can_dismiss is false)
+--     [option]
+--         image=(image path) (optional, will use misc/blank-hex.png if missing)
+--         title=(string)
+--         text=(string)
+--     [/option]
+-- [/item_choice_dialog]
+---
+function wesnoth.wml_actions.item_choice_dialog(cfg)
+	-- Some early sanity checking.
+	if not helper.get_child(cfg, "option") then
+		helper.wml_error("[item_choice_dialog]: missing mandatory [option] children")
+	end
+
+	local variable = nil
+
+	local can_dismiss = cfg.can_dismiss
+
+	if can_dismiss == nil then
+		can_dismiss = true
+	elseif not can_dismiss then
+		variable = cfg.variable or "choice"
+	end
+
+	-- HACK: we need to generate a multi_page widget's page_data node early on,
+	-- so we have to go over the [entry] children a first time as well, to get
+	-- the strings.
+
+	local page_data = {}
+	local page_count = 0
+
+	for option in helper.child_range(cfg, "option") do
+		local title = option.title or "-"
+		local text = option.text or ""
+
+		page_count = page_count + 1
+
+		page_data[page_count] = T.row {
+			T.column { label = title .. ":\n\n" .. text },
+		}
+	end
+
+	local list_definition = {
+		T.row {
+			T.column {
+				vertical_grow = true,
+				horizontal_grow = true,
+				T.toggle_panel {
+					definition = "default",
+					return_value_id = "ok",
+					T.grid {
+						T.row {
+							T.column {
+								grow_factor = 1,
+								horizontal_grow = true,
+								border = "all",
+								border_size = 5,
+								T.image {
+									id = "option_icon",
+									linked_group = "option_icon",
+								}
+							},
+							T.column {
+								grow_factor = 1,
+								horizontal_grow = true,
+								border = "all",
+								border_size = 5,
+								T.label {
+									id = "option_title",
+									linked_group = "option_title",
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	} -- end list_definition
+
+	-- #textdomain wesnoth
+	_ = wesnoth.textdomain "wesnoth"
+
+	local ok_label = _ "Close"
+
+	if not can_dismiss then
+		ok_label = _ "OK"
+	end
+
+	local control_buttons_row = {
+		T.column {
+			border = "all", border_size = 5,
+			T.button { id = "ok", label = ok_label }
+		}
+	}
+
+	if not can_dismiss then
+		control_buttons_row[2] = T.column {
+			border = "all", border_size = 5,
+			T.button { id = "cancel", label = _ "Cancel" }
+		}
+	end
+
+	local dialog_definition = {
+		automatic_placement = false, -- handle all geometry here
+
+		click_dismiss = false,
+
+		width = 640,	-- should be < 800 (minimum x res)
+		height = 400,	-- should be < 480 (minimum y res)
+
+		x = "(screen_width / 2 - 640 / 2)",
+		y = "(screen_height / 2 - 400 / 2)",
+
+		T.helptip { id = "tooltip_large" },
+		T.tooltip { id = "tooltip_large" },
+
+		T.linked_group { id = "option_icon", fixed_width = true },
+		T.linked_group { id = "option_title", fixed_width = true },
+
+		T.grid {
+			T.row {
+				grow_factor = 0,
+				T.column {
+					border = "all", border_size = 5,
+					horizontal_alignment = "left",
+					T.label {
+						id = "title",
+						definition = "title",
+						wrap = true,
+					}
+				}
+			},
+			T.row {
+				grow_factor = 0,
+				T.column {
+					border = "all", border_size = 5,
+					horizontal_alignment = "left",
+					T.label {
+						id = "text",
+						wrap = true,
+					}
+				}
+			},
+			T.row {
+				grow_factor = 1,
+				T.column {
+					T.grid {
+						T.row {
+							T.column {
+								grow_factor = 1,
+								border = "all", border_size = 5,
+								horizontal_alignment = "left",
+								vertical_alignment = "top",
+								T.listbox {
+									id = "option_list",
+									definition = "default",
+									T.list_definition(list_definition),
+								}
+							},
+							T.column {
+								grow_factor = 2,
+								horizontal_alignment = "left",
+								vertical_alignment = "top",
+								T.multi_page {
+									id = "current_option_pager",
+									T.page_definition {
+										T.row {
+											T.column {
+												border = "all", border_size = 5,
+												horizontal_alignment = "left",
+												vertical_alignment = "top",
+												T.scroll_label {
+													id = "current_option_text",
+												}
+											}
+										}
+									},
+									T.page_data(page_data),
+								}
+							}
+						}
+					}
+				}
+			},
+			T.row {
+				grow_factor = 0,
+				T.column {
+					horizontal_alignment = "right",
+					T.grid {
+						T.row(control_buttons_row)
+					}
+				}
+			}
+		}
+	} -- end dialog_definition
+
+	local function on_select()
+		local i = wesnoth.get_dialog_value("option_list")
+
+		if(i > page_count) then
+			wesnoth.fire("wml_message", {
+				logger = "error",
+				message = "[AtS] BUG: invalid option_list row number"
+			})
+
+			return
+		end
+
+		wesnoth.set_dialog_value(i, "current_option_pager")
+	end
+
+	local function preshow()
+		if cfg.title then
+			wesnoth.set_dialog_value(cfg.title, "title")
+		end
+
+		if cfg.text then
+			wesnoth.set_dialog_value(cfg.text, "text")
+		end
+
+		local i = 1
+
+		for entry in helper.child_range(cfg, "option") do
+			local image = entry.image or "misc/blank-hex.png"
+			local title = entry.title or "-"
+			local text = entry.text or ""
+
+			wesnoth.set_dialog_value(image, "option_list", i, "option_icon")
+			wesnoth.set_dialog_value(title, "option_list", i, "option_title")
+
+			i = i + 1
+		end
+
+		wesnoth.set_dialog_callback(on_select, "option_list")
+
+		-- Select the first entry by default.
+		wesnoth.set_dialog_value(1, "option_list")
+		on_select()
+	end
+
+
+	local res = wesnoth.synchronize_choice(function()
+		local choice = -1
+		local retval = wesnoth.show_dialog(
+			dialog_definition, preshow,
+			function() choice = (wesnoth.get_dialog_value("option_list") - 1) end
+		)
+		return { status = retval, value = choice }
+	end)
+
+	if variable ~= nil then
+		if res.status == -2 then
+			wesnoth.set_variable(variable, -1)
+		else
+			wesnoth.set_variable(variable, res.value)
+		end
+	end
+end
