d071c566d4bbbaaab94c53e7548af1419a5412d7
Ignacio R. Morelle <shadowm@wesnoth.org>
2014-03-04 22:42:28 -0300

    Allow Wesnoth 1.11.8 as an unsupported but working version

--- lua/version.lua
+++ lua/version.lua
@@ -14,5 +14,5 @@ local v = wesnoth.compare_versions
 local _ = wesnoth.textdomain "wesnoth-After_the_Storm"
 
 if v(ver, '<', '1.11.11') then
-	do_bug( _ "After the Storm requires Wesnoth 1.11.11 or later. Version 1.11.9 (but not 1.11.10) may also work; however, no support can be provided by the campaign maintainer in the forums.", v(ver, '==', '1.11.9'))
+	do_bug( _ "After the Storm requires Wesnoth 1.11.11 or later. Versions 1.11.8 and 1.11.9 (but not 1.11.10) may also work; however, no support can be provided by the campaign maintainer in the forums.", v(ver, '>=', '1.11.8') and v(ver, '<=', '1.11.9'))
 end
