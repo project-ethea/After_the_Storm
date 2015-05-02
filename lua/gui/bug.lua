b0c2d6aef7216979465815c6e7dd91e8b91d9508
Ignacio R. Morelle <shadowm@wesnoth.org>
2015-05-02 06:08:00 -0300

    Don't allow bypassing may_ignore=false [bug]s by pressing Enter

--- lua/gui/bug.lua
+++ lua/gui/bug.lua
@@ -192,7 +192,7 @@ function wesnoth.wml_actions.bug(cfg)
 		wesnoth.wml_actions.inspect {}
 	end
 
-	if dialog_result == 2 then
+	if dialog_result == 2 or not may_ignore then
 		wesnoth.fire("endlevel", {
 			result = "defeat",
 			linger_mode = false
