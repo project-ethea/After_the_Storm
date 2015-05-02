1decc6d3afc3a4bb27c2bcff0cb4236d6f77b56e
Ignacio R. Morelle <shadowm@wesnoth.org>
2015-05-01 23:03:26 -0300

    Document Lua lib stuff

--- lua/lib.lua
+++ lua/lib.lua
@@ -2,6 +2,12 @@
 -- Lua utilities library
 ---
 
+---
+-- Returns a pseudorandom value from a set.
+--
+-- This uses the same format and implementation as WML [set_variable], so that
+-- the function is MP and replay-safe (unlike Lua's math.random).
+---
 function safe_random(arg)
 	wesnoth.fire("set_variable", {
 		name = "temp_ats_lua_random",
@@ -14,13 +20,23 @@ function safe_random(arg)
 	return r
 end
 
-W_ERR  = 1
-W_WARN = 2
-W_INFO = 3
-W_DBG  = 4
+---
+-- Log levels for wput and wprintf.
+---
+
+W_ERR  = 1 -- Error.
+W_WARN = 2 -- Warning (default maximum log level).
+W_INFO = 3 -- Info.
+W_DBG  = 4 -- Debug.
 
 local loglvl_map = { "error", "warning", "info", "debug" }
 
+---
+-- Prints a text line using the engine log facilities.
+--
+-- lvl: One of W_ERR, W_WARN, W_INFO, or W_DBG.
+-- msg: Line contents.
+---
 function wput(lvl, msg)
 	wesnoth.wml_actions.wml_message({
 		logger = loglvl_map[math.max(1, math.min(lvl, #loglvl_map))],
@@ -28,10 +44,24 @@ function wput(lvl, msg)
 	})
 end
 
+---
+-- Prints a formatted (printf-style) line using the engine log facilities.
+--
+-- lvl: One of W_ERR, W_WARN, W_INFO, or W_DBG.
+-- fmt: Line format specification.
+-- ...: Parameters for line formatting.
+---
 function wprintf(lvl, fmt, ...)
 	wput(lvl, string.format(fmt, ...))
 end
 
+---
+-- Returns a textdomain-specific string translated to the current locale
+--
+-- Note that this is only intended to be used with mainline textdomains. For
+-- add-on textdomains the _ hack including comments with fake WML is still
+-- needed so that wmlxgettext can catch and add the new strings.
+---
 function wgettext(str, domain)
 	if domain == nil then
 		domain = "wesnoth"
