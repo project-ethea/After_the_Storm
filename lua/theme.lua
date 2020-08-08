9ba3fe816139140db01887f211ffcee9446687d4
Iris Morelle <shadowm@wesnoth.org>
2020-08-08 00:41:45 -0400

    lua: Use register_unit_status_display()

--- lua/theme.lua
+++ lua/theme.lua
@@ -4,43 +4,32 @@
 
 -- #textdomain wesnoth-After_the_Storm
 local _ = wesnoth.textdomain "wesnoth-After_the_Storm"
-local old_unit_status = wesnoth.theme_items.unit_status
 
-function wesnoth.theme_items.unit_status()
-	local u = wesnoth.get_displayed_unit()
-	if not u then return {} end
-	local s = old_unit_status()
+register_unit_status_display {
+	id      = "stunned",
+	icon    = "misc/stunned-status-icon.png",
+	tooltip = _ "stunned: This unit is stunned. It cannot enforce its Zone of Control.",
+}
 
-	if u.variables.uu_immobilized then
-		-- FIXME: uu_immobilized should probably be promoted to an actual
-		--        [status] member at some point, but a lot of code in
-		--        unit-utils.cfg needs to be changed for that purpose.
-		table.insert(s, { "element", {
-			image = "misc/bound-status-icon.png",
-			tooltip = _ "bound: This unit is bound to the movement range of another unit by a spell."
-		}})
-	end
+register_unit_status_display {
+	id      = "dazed",
+	icon    = "misc/dazed-status-icon.png",
+	tooltip = _ "dazed: This unit is dazed. It suffers a -10% penalty to both its defense and chance to hit (except for magical attacks).",
+}
 
-	if u.status.stunned then
-		table.insert(s, { "element", {
-			image = "misc/stunned-status-icon.png",
-			tooltip = _ "stunned: This unit is stunned. It cannot enforce its Zone of Control."
-		}})
-	end
+register_unit_status_display {
+	id      = "absconding",
+	icon    = "misc/absconding-status-icon.png",
+	tooltip =  _ "absconding: This unit made use of its absconding ability during its previous turn.",
+}
 
-	if u.status.dazed then
-		table.insert(s, { "element", {
-			image = "misc/dazed-status-icon.png",
-			tooltip = _ "dazed: This unit is dazed. It suffers a -10% penalty to both its defense and chance to hit (except for magical attacks)."
-		}})
-	end
+-- FIXME: uu_immobilized should probably be promoted to an actual
+--        [status] member at some point, but a lot of code in
+--        unit-utils.cfg needs to be changed for that purpose.
 
-	if u.status.absconding then
-		table.insert(s, { "element", {
-			image = "misc/absconding-status-icon.png",
-			tooltip = _ "absconding: This unit made use of its absconding ability during its previous turn."
-		}})
-	end
-
-	return s
-end
+register_unit_status_display {
+	id      = "uu_immobilized",
+	icon    = "misc/bound-status-icon.png",
+	tooltip = _ "bound: This unit is bound to the movement range of another unit by a spell.",
+	source  = "variables"
+}
